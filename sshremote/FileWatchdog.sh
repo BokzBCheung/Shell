8822b3f592f7c763c56ebfa2a846cbd5b016cdc0
BokzBCheung <BokzBCheung@gmail.com>
2018-07-30 10:41:25 +0800

    update the Dual machine's scripts

--- sshremote/FileWatchdog.sh
+++ sshremote/FileWatchdog.sh
@@ -3,12 +3,47 @@
 # date 2018-6-14 17:06:36
 
 # show the capacity of the /dev/vda1 
-df -h|awk '/vda1/ {print $5}' > capacity.txt
+df -h|awk '/vda1/ {print $5}' > capacity18.txt
 
-capacity=$(cat capacity.txt)
+capacity=$(cat capacity18.txt)
 
 # write the info to file
-echo "your machine 18's capacity is $capacity !" >capacity.txt
+echo "your machine 18's capacity is $capacity !" >capacity18.txt
 
-# write the machine to file 
-df -h > capacityofmachine.txt
+# write the machine to file
+df -h > capacityofmachine18.txt
+
+# make a judgement
+if [ ${capacity%?} -ge 80 ];then
+echo "your machine 18's available capacity is more than  80%,System will clear the logs file,Please attention check！" >> capacity18.txt
+
+# clear the logs file
+echo "=============================clear logs file start=============================" >> capacity18.txt
+echo "clear mecury_b's file..." >> capacity18.txt
+rm -rf /home/mecury_b/apache-tomcat-8.0.47/logs/*.txt
+rm -rf /home/mecury_b/apache-tomcat-8.0.47/logs/catalina.out
+rm -rf /home/mecury_b/apache-tomcat-8.0.47/logs/bussiness
+echo "complete mecury_b's file" >> capacity18.txt
+
+echo "clear mecury_c's file..." >> capacity18.txt
+rm -rf /home/mecury_c/apache-tomcat-8.0.47/logs/*.txt
+rm -rf /home/mecury_c/apache-tomcat-8.0.47/logs/catalina.out
+rm -rf /home/mecury_c/apache-tomcat-8.0.47/logs/billingCounter
+echo "complete mecury_c's file" >> capacity18.txt
+
+echo "clear mecury_p's file..." >> capacity18.txt
+rm -rf /home/mecury_p/apache-tomcat-8.0.47/logs/*.txt
+rm -rf /home/mecury_p/apache-tomcat-8.0.47/logs/catalina.out
+rm -rf /home/mecury_p/apache-tomcat-8.0.47/logs/ioCounter
+echo "complete mecury_p's file" >> capacity18.txt
+
+echo "clear mecury_r's file..." >> capacity18.txt
+rm -rf /home/mecury_r/apache-tomcat-8.0.47/logs/*.txt
+rm -rf /home/mecury_r/apache-tomcat-8.0.47/logs/catalina.out
+rm -rf /home/mecury_r/apache-tomcat-8.0.47/logs/saasmercury
+echo "complete mecury_r' file" >> capacity18.txt
+echo "==============================clear logs file end==============================" >> capacity18.txt
+
+else
+echo "your machine 18's available capacity is enough!" >> capacity18.txt
+fi
--- sshremote/SSHCapacity.sh
+++ sshremote/SSHCapacity.sh
@@ -3,15 +3,36 @@
 # date 2018-6-15 14:23:02
 
 # remotelogin the 18 machine
+################################################################################################################################
 ssh root@10.10.10.18 << remotessh
 
 # run the script of the remote machine
 "/root/FileWatchdog.sh"
 exit
 remotessh
+###############################################################################################################################
+
+# remotelogin the 19 machine
+###############################################################################################################################
+ssh root@10.10.10.19 << remotessh
+
+#run the script of the remote machine
+"/root/FileWatchdog.sh"
+exit
+remotessh
+###############################################################################################################################
 
 # scp the remote file to local machine
-scp root@10.10.10.18:/root/\{capacity.txt,capacityofmachine.txt\} /root/
+scp root@10.10.10.18:/root/\{capacity18.txt,capacityofmachine18.txt\} /root/txtOfSSH
+scp root@10.10.10.19:/root/\{capacity19.txt,capacityofmachine19.txt\} /root/txtOfSSH
+
+# construct a summary file
+cat /root/txtOfSSH/capacity18.txt > capacity.txt
+cat /root/txtOfSSH/capacity19.txt >> capacity.txt
+echo "10.10.10.18 Machine disk running status:" > capacityofmachine.txt
+cat /root/txtOfSSH/capacityofmachine18.txt >> capacityofmachine.txt
+echo "10.10.10.19 Machine disk running status:" >> capacityofmachine.txt
+cat /root/txtOfSSH/capacityofmachine19.txt >> capacityofmachine.txt
 
 # mail to admin show the machine status
-cat capacity.txt|mail -s "世茂服务器--机器(/dev/vda1)磁盘信息" -a /root/capacityofmachine.txt zb@reformer.com.cn
+cat /root/capacity.txt|mail -s "世茂服务器--18,19机器(/dev/vda1)磁盘信息" -a /root/capacityofmachine.txt zb@reformer.com.cn
