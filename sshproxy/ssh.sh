581baa9be94650f4d56fb017e4fdd4f5b952d27f
BokzBCheung <BokzBCheung@gmail.com>
2018-11-22 14:39:39 +0800

    add proxy file

---
+++ sshproxy/ssh.sh
@@ -0,0 +1,5 @@
+ssh -f -g -L 5306:rm-c8di90oz97w447b97.mysql.rds.aliyuncs.com:3306 root@172.22.255.148 sleep 720000000
+ssh -f -g -L 6306:rm-c8dcak7zg7cbp442t.mysql.rds.aliyuncs.com:3306 root@172.22.255.148 sleep 720000000
+ssh -f -g -L 7306:rm-c8dqh6bqm0ppkwdju.mysql.rds.aliyuncs.com:3306 root@172.22.255.148 sleep 720000000
+ssh -f -g -L 8306:rm-c8d49jv6mlts3t72s.mysql.rds.aliyuncs.com:3306 root@172.22.255.148 sleep 720000000
+ssh -f -g -L 9306:rm-c8d7c556xcrf567np.mysql.rds.aliyuncs.com:3306 root@172.22.255.148 sleep 720000000
